c5f19265184af66f5e541c9cecf4bbe0fc880022
tristanbenier <tristan@allocadeau.com>
2017-08-16 15:43:34 +0200

    Ajout de liens dans la page /formations

--- web-app/src/views/training/index.php
+++ web-app/src/views/training/index.php
@@ -1,6 +1,8 @@
 <h1>Formations</h1>
 
 <ul>
-  <li><a href="/formations/gerer">Gérer les formations</a></li>
-  <li><a href="/formations/presence">Feuilles de présence</a></li>
+  <li><a href="/formations">Ajouter une compétence au référentiel (R.US02)</a></li>
+  <li><a href="/formations">Gérer les compétences des formateurs (R.US03)</a></li>
+  <li><a href="/formations/gerer">Gérer les formations (R.US04)</a></li>
+  <li><a href="/formations/presence">Feuilles de présence (R.US09)</a></li>
 </ul>
